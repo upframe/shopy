32c5aac94e8fd4e78389105628e80bfe87808e29
Henrique Dias <hacdias@gmail.com>
2016-08-25 22:46:59 +0100

    more idiomatic stuff

--- upframe.go
+++ upframe.go
@@ -7,16 +7,29 @@ import (
 	"github.com/mholt/caddy/caddyhttp/httpserver"
 )
 
+// Upframe is the startup struct
 type Upframe struct {
 	Next httpserver.Handler
 }
 
 func (u Upframe) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
-	if !strings.HasPrefix(r.URL.Path, "/assets") {
+	if shouldHandle(r.URL.Path) {
 		w.Write([]byte("We are Upframe"))
 
 		return http.StatusOK, nil
 	}
 
 	return u.Next.ServeHTTP(w, r)
+}
+
+func shouldHandle(path string) bool {
+	paths := []string{"/css", "/js", "/imgs"}
+
+	for i := range paths {
+		if strings.HasPrefix(path, paths[i]) {
+			return false
+		}
+	}
+
+	return true
 }
