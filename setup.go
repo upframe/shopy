ef7886ec68a927dd960830523a16e5dd950fe295
Henrique Dias <hacdias@gmail.com>
2016-08-29 12:54:49 +0100

    gets database connection info from Caddyfile

--- setup.go
+++ setup.go
@@ -14,16 +14,62 @@ func init() {
 }
 
 func setup(c *caddy.Controller) error {
+	// Initializes DB connection information
+	dbUser := "root"
+	dbPass := "root"
+	dbHost := "127.0.0.1"
+	dbPort := "3306"
+	dbName := "upframe"
+
+	// Gets the options from the Caddyfile
+	for c.Next() {
+		for c.NextBlock() {
+			switch c.Val() {
+			case "db_user":
+				if !c.NextArg() {
+					return c.ArgErr()
+				}
+
+				dbUser = c.Val()
+			case "db_pass":
+				if !c.NextArg() {
+					return c.ArgErr()
+				}
+
+				dbPass = c.Val()
+			case "db_host":
+				if !c.NextArg() {
+					return c.ArgErr()
+				}
+
+				dbHost = c.Val()
+			case "db_port":
+				if !c.NextArg() {
+					return c.ArgErr()
+				}
+
+				dbPort = c.Val()
+			case "db_name":
+				if !c.NextArg() {
+					return c.ArgErr()
+				}
+
+				dbName = c.Val()
+			}
+		}
+	}
+
+	// Connects to the database and checks for an error
+	err := models.InitDB(dbUser, dbPass, dbHost, dbPort, dbName)
+	if err != nil {
+		return err
+	}
+
+	// Adds the middleware to Caddy
 	mid := func(next httpserver.Handler) httpserver.Handler {
 		return Upframe{
 			Next: next,
 		}
-	}
-
-	// TODO: get variables from caddyfile
-	err := models.InitDB("root", "root", "127.0.0.1", "3306", "upframe")
-	if err != nil {
-		return err
 	}
 
 	httpserver.GetConfig(c).AddMiddleware(mid)
