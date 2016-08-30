6ecfd9fa3828bd4049522f694e54a777f65e38d2
fabioswartz <ffcf.pt.en@gmail.com>
2016-08-31 00:14:24 +0100

    forgot one line in store.go

--- pages/store.go
+++ pages/store.go
@@ -4,12 +4,9 @@ import (
 	"net/http"
 
 	"github.com/gorilla/sessions"
-	"github.com/hacdias/upframe/models"
 )
 
 // StoreGET handles the GET request for /store page
 func StoreGET(w http.ResponseWriter, r *http.Request, s *sessions.Session) (int, error) {
-	models.DeleteByID(1)
-
 	return RenderHTML(w, nil, "store")
 }
